57d554dff32e342757c16c5dd73eaadc6c2524ef
jjfedun <jjfedun@ualberta.ca>
2024-11-02 19:51:48 -0600

    User Class created

--- titans_project/app/src/main/java/com/example/titans_project/User.java
+++ titans_project/app/src/main/java/com/example/titans_project/User.java
@@ -1,4 +1,43 @@
 package com.example.titans_project;
 
 public class User {
+
+    private String name;
+    private String email;
+    private String phone_number = "";  // Optional, empty by default
+
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public User(String name, String email, String phone_number){
+        this.name = name;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPhoneNumber(){
+        return this.phone_number;
+    }
+
+    public void setPhoneNumber(){
+        this.phone_number = phone_number;
+    }
 }
--- titans_project/app/src/main/java/com/example/titans_project/Waitlist.java
+++ titans_project/app/src/main/java/com/example/titans_project/Waitlist.java
@@ -5,5 +5,5 @@ import java.util.List;
 public class Waitlist {
     private String event;
     private String organizer;
-    private List<?> waitlist;
+    private List<User> waitlist;
 }
