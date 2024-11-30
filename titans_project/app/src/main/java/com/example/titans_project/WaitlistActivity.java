2e6db7e4a75b885f8784a8168dd560d8b5e7330a
jjfedun <jjfedun@ualberta.ca>
2024-11-30 15:11:36 -0700

    fixed my imageButton bug

--- titans_project/app/src/main/java/com/example/titans_project/WaitlistActivity.java
+++ titans_project/app/src/main/java/com/example/titans_project/WaitlistActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,7 +34,7 @@ public class WaitlistActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waitlist);
 
-        Button sendNotification = findViewById(R.id.buttonSendNotification);
+        ImageButton sendNotification = findViewById(R.id.buttonSendNotification);
         Button generateLottery = findViewById(R.id.buttonGenerateLottery);
 
         waitlistRecyclerView = findViewById(R.id.waitlistRecyclerView);
