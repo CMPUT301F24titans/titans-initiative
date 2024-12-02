90d3463bd3a0f9ba22b2938ac51e54c70b7fe2f2
jjfedun <jjfedun@ualberta.ca>
2024-12-01 18:59:34 -0700

    remove unused generateLottery method in Attendee

--- titans_project/app/src/main/java/com/example/titans_project/Attendee.java
+++ titans_project/app/src/main/java/com/example/titans_project/Attendee.java
@@ -29,35 +29,6 @@ public class Attendee {
     }
 
     /**
-     * Generates lottery for a waitlist
-     * @param waitList
-     *  The current waitlist to get into an event
-     * @param selectAmount
-     *  The maximum number of users allowed to be selected into the event
-     * @return
-     *  The hash table of attendee
-     */
-    public HashMap<String, String> generateLottery(HashMap<String, String> waitList, Integer selectAmount) {
-        // Check if the select amount exceeds the number of user_ids in the map
-        if (selectAmount > waitList.size()) {
-            return waitList;  // return wishlist if there is less people than the selection amount
-        }
-        // Convert the user_ids of the HashMap into a list
-        List<String> userIDs = new ArrayList<>(waitList.keySet());
-        // Shuffle the list of user_ids for random selection
-        Collections.shuffle(userIDs);
-        // Create a HashMap to store selected users
-        HashMap<String, String> selectedUsers = new HashMap<>();
-
-        // Select the required number of user_ids and add them to the result map
-        for (int i = 0; i < selectAmount; i++) {
-            String key = userIDs.get(i);
-            selectedUsers.put(key, waitList.get(key));
-        }
-        return selectedUsers;
-    }
-
-    /**
      * This set the name of attendee
      * @param name
      *  The name of attendee
